6a2fd9c4abdcb7778feefc8dd7697a4a9d5145b2
Salvatore <antoniosalvatores@gmail.com>
2022-10-08 15:54:30 -0800

    Update
    nuevos xml para leer

--- Script/Asstes/tree.py
+++ Script/Asstes/tree.py
@@ -1,5 +1,4 @@
-import re
-from unicodedata import name
+import re as RE
 from xml.etree import ElementTree as ET
 import os as OS
 import tkinter as tk
@@ -17,39 +16,41 @@ space_buttons = 50 #space between buttons
 #--------------------------------Script------------------------------------#
 #list with all songs in xml
 def print_all_songs():
-    for i in range(len(root[0])):
-        name = root[0][i][0].text
-        size = len(root[0][i][31])
+    for i in range(len(root_gameinfo[0])):
+        name = root_gameinfo[0][i][0].text
+        size = len(root_gameinfo[0][i][31])
         print('_____________________________')
         print('Name: ' + name)
         print('Size :' +str(size))
-        for x in root[0][i][31]:
+        for x in root_gameinfo[0][i][31]:
             print(x.text)
 
-def write_xml(list_of_songs,era_folder):
-    if len(root[0]) != len(epocas):
+def write_xml_gameinfo(list_of_songs,era_folder):
+    if len(root_gameinfo[0]) != len(epocas):
         print("tag_era are missing, the tag_era found are:")
-        for tag_EraInfo in root[0]:
+        for tag_EraInfo in root_gameinfo[0]:
             print(tag_EraInfo[0].text)                
     else:
         # get the index of era_folder
         index = epocas.index(era_folder)
-        if root[0][index][31].tag == (ns + 'EraInfoSoundtracks'):
+        if root_gameinfo[0][index][31].tag == (ns + 'EraInfoSoundtracks'):
             #clear songs
-            for element in root[0][index][31]:
-                root[0][index][31].remove(element)
+            for element in root_gameinfo[0][index][31]:
+                root_gameinfo[0][index][31].remove(element)
+                print(root_gameinfo[0][index][31].tag)
+                print(len(root_gameinfo[0][index][31]))
             #new songs
             for song in list_of_songs:
                 name_song = OS.path.splitext(song)[0] 
-                refactor_name = re.sub(r"[^a-zA-Z0-9]", "", str(name_song))     # Remove Special Characters from a String Using re.sub()           
+                refactor_name = RE.sub(r"[^a-zA-Z0-9]", "", str(name_song))     # Remove Special Characters from a String Using re.sub()           
                 new_song=ET.Element(song_tag)
                 new_song.text= pref + refactor_name.upper()
-                root[0][index][31].append(new_song)
+                root_gameinfo[0][index][31].append(new_song)
         else:
             print("EraInfoSoundtracks not found")
     try:
         ET.register_namespace('',"x-schema:CIV4GameInfoSchema.xml")
-        tree.write('output.xml')
+        tree_gameinfo.write('output.xml')
         #print_all_songs()
     except:
         print("Something went wrong when writing to the file")
@@ -58,19 +59,23 @@ def write_xml(list_of_songs,era_folder):
 def directory():
     #path of files
     path = path = OS.getcwd()
-    print(path)
+    #print(path)
     return path
 
 def list_of_songs_get(era_folder):
-    #arr of dictory and files in the current folder
-    list_of_songs = OS.listdir(directory() + '\Asstes\Sounds' + chr(92) + era_folder)
+    #arr of dictory and files in the soundtracks folder
+    list_of_songs = OS.listdir(directory() + '\Asstes\Sounds\Soundtracks' + chr(92) + era_folder)
     print(list_of_songs)
     return list_of_songs
 #------------------------------------------------------------------------#
 
 #--------------------------parsing directly------------------------------#
-tree = ET.parse(directory() + '\Asstes\XML\GameInfo\CIV4EraInfos.xml')
-root = tree.getroot()
+tree_gameinfo = ET.parse(directory() + '\Asstes\XML\GameInfo\CIV4EraInfos.xml')
+root_gameinfo = tree_gameinfo.getroot()
+tree_audio2dscripts = ET.parse(directory() + '\Asstes\XML\Audio\Audio2DScripts.xml')
+root_audio2dscripts = tree_audio2dscripts.getroot()
+tree_audiodefines = ET.parse(directory() + '\Asstes\XML\Audio\AudioDefines.xml')
+root_audiodefines = tree_audiodefines.getroot()
 #------------------------------------------------------------------------#
 
 #------------------------------------Menu----------------------------------#
@@ -88,7 +93,7 @@ for x in range(len(epocas)):
     def action (epoca = epocas[x]):
         label1 = tk.Label(rootTK, text= epoca, fg='blue', font=('helvetica', 12, 'bold'))
         canvas1.create_window(150, 550, window=label1)
-        write_xml(list_of_songs_get(epoca),epoca)
+        write_xml_gameinfo(list_of_songs_get(epoca),epoca)
     button = tk.Button(text=epocas[x], command=action, bg='brown',fg='white')
     #buttons.append(button)
     canvas1.create_window(150, 150 + x*space_buttons, window=button)
